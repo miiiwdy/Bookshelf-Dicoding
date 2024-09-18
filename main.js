f89d1a408d96a03a1bdbe29af5d5b7dda7ef9648
miiiwdy <fahmiwidyaa@gmail.com>
2024-09-18 13:25:23 +0700

    fix minor #2

--- main.js
+++ main.js
@@ -65,23 +65,24 @@ function createBookElement(buku) {
     const bookItem = document.createElement("article");
     bookItem.classList.add("book_item");
     bookItem.innerHTML = `
+    <div data-bookid="${buku.id}" data-testid="bookItem">
     <h3 data-testid="bookItemTitle">${buku.title}</h3>
     <p data-testid="bookItemAuthor"><strong>Penulis:</strong> ${buku.author}</p>
     <p data-testid="bookItemYear"><strong>Tahun:</strong> ${buku.year}</p>
-    <div>
+    <div id="buku_btn" >
         ${buku.isComplete ? `
-            <svg id="updateBelumSelesai" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" onclick="updateStatusBuku(${buku.id})">
+            <svg id="updateBelumSelesai" data-testid="bookItemIsNotCompleteButton" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" onclick="updateStatusBuku(${buku.id})">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
         ` : `
-            <svg id="updateSelesai" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" onclick="updateStatusBuku(${buku.id})">
+            <svg id="updateSelesai" data-testid="bookItemIsCompleteButton" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" onclick="updateStatusBuku(${buku.id})">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
         `}
-        <svg id="hapusBuku" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" onclick="hapusBuku(${buku.id})">
+        <svg id="hapusBuku" data-testid="bookItemDeleteButton" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" onclick="hapusBuku(${buku.id})">
             <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
         </svg>
-        <svg id="editBuku" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" onclick="editBuku(${buku.id})">
+        <svg id="editBuku" data-testid="bookItemEditButton" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" onclick="editBuku(${buku.id})">
             <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
         </svg>
     </div>
